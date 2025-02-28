abdcb06d7bcc0536ef8a34303cfe7693d78dfb1e
prathamnaik12 <148609885+prathamnaik12@users.noreply.github.com>
2025-02-28 11:58:21 +0530

    loginnregisterpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 import Register from "./pages/register"
 import { Routes, Route } from "react-router-dom"
+import Login from "./pages/login"
 function App() {
 
   return (
-    <div className="md:flex justify-center items-center h-full w-full ">
+    <div className="md:flex justify-center items-center h-screen w-full ">
       <Routes>
         <Route path="/register" element={<Register />} />
-
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   )
