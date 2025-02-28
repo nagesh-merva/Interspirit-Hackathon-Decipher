43de8adc3e6ecd1c76139639912676462b97b86c
prathamnaik12 <148609885+prathamnaik12@users.noreply.github.com>
2025-03-01 00:38:54 +0530

    landingpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,18 @@ import { Routes, Route } from "react-router-dom"
 import Login from "./pages/login"
 import Dashboard from "./pages/Dashboard"
 import SentimentTrends from "./pages/SentimentTrends"
+import LandingPage from "./pages/Home"
 function App() {
 
   return (
     <div className="md:flex justify-center items-center h-screen w-full">
       <Routes>
+        <Route index path="/" element={<LandingPage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/sentiment" element={<SentimentTrends />} />
+
       </Routes>
     </div>
   )
