9be577b2b86815551cc51b97aedace6526ecdd9f
prathamnaik12 <148609885+prathamnaik12@users.noreply.github.com>
2025-02-28 11:02:02 +0530

    drt

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,10 @@
+function Register() {
+    return (
+        <div className="md:flex justify-center items-center h-full w-full ">
+            <div>
+                <h1 className="text-2xl font-bold" >Create Account</h1>
+            </div>
+        </div>
+    )
+}
+export default Register
